97085e36032585b5ebb9117ec5af4a26af47e1bd
NaGiARaShi <wcd140022@gmail.com>
2026-08-09 22:18:42 +0800

    feat(router): 将路由模式从 hash 切换到 history
    - 将 createWebHashHistory 替换为 createWebHistory
    - 更新路由器配置以使用 HTML5 History 模式
    - 修改文档中的路由模式描述以保持同步

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
 
 const TOKEN_KEY = 't-utils-auth-token'
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: [
         {path: '/', component: Home},
         {
